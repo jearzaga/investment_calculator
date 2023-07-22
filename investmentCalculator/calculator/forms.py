155c5f0079303b7d107e8862f113bcb3642fde1b
John Erick Arzaga <johnerickarzaga11@gmail.com>
2023-07-22 19:21:36 +0800

    added design to the home page

--- investmentCalculator/calculator/forms.py
+++ investmentCalculator/calculator/forms.py
@@ -5,3 +5,15 @@ class InvestmentForm(forms.Form):
     number_of_years = forms.IntegerField(label='Number of Years', min_value=0)
     return_rate = forms.FloatField(label='Return Rate', min_value=0, max_value=100)
     annual_additional_contribution = forms.FloatField(label='Annual Additional Contribution', min_value=0)
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        fields_to_update = [
+            'starting_amount',
+            'number_of_years',
+            'return_rate',
+            'annual_additional_contribution'
+        ]
+
+        for field_name in fields_to_update:
+            self.fields[field_name].widget.attrs.update({'class': 'form-control'})
